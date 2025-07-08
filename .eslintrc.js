7d75d6355367d60a61ddf4888fb78579341b01a8
Cozy Kwon <cozy@bucketplace.net>
2025-07-08 23:41:54 +0900

    프로젝트 초기 환경 설정 (#2)
    * chore: pnpm 설정
    
    * chore: 불필요 초기 파일 제거
    
    * config: eslint 규칙 기본 설정
    
    * chore: quotes 관련 lint 설정 변경
    
    * deps: emotion 설치 및 기본값 적용
    
    * chore: eslintrc 일부 수정
    
    * chore: pull_request_template 추가
    
    * chore: typo

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'eol-last': 'warn',
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'no-extra-semi': 'warn',
+    'comma-dangle': ['warn', 'always-multiline'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
