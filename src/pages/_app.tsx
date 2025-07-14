ca0f79dc1dd8c1feaafb248d945e84e7954db18d
Cozy Kwon <cozy@bucketplace.net>
2025-07-14 21:29:58 +0900

    feat: _app.tsx에 실행부 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import { Global } from '@emotion/react';
 
 import { globalStyles } from '@/common';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('@/mocks');
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
