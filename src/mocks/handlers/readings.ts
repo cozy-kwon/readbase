d82deafbd8d52aa368aeef788c33f168d65edc83
Cozy Kwon <cozy@bucketplace.net>
2025-07-14 21:29:13 +0900

    feat: mock api 간단하게 추가

---
+++ src/mocks/handlers/readings.ts
@@ -0,0 +1,13 @@
+import { http, HttpResponse } from 'msw';
+
+import { Reading } from '@/models';
+
+export const readingHandlers = [
+  http.post('/reading', async ({ request }) => {
+    const data = await request.json() as Reading;
+
+    // TODO: 최소한의 서버 에러 처리?
+
+    return HttpResponse.json(data, { status: 201 });
+  }),
+];
