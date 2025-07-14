aa95edcdfa99dc4d81974c5acec8eb7f96f7f42c
Cozy Kwon <cozy@bucketplace.net>
2025-07-14 22:11:36 +0900

    feat: QueryclientProvider 래핑

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { globalStyles } from '@/common';
 
@@ -8,11 +9,13 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('@/mocks');
 }
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
