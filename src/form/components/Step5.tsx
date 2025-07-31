9be356e2abef7b3ff6969ccd0b2b4b93ad70e8d5
Cozy Kwon <cozy@bucketplace.net>
2025-07-31 23:56:55 +0900

    feat: Step5 추가

--- src/form/components/Step5.tsx
+++ src/form/components/Step5.tsx
@@ -1,7 +1,41 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import styled from '@emotion/styled';
+
+import { SelectField } from '@/components';
+import { ReadingForm } from '@/form';
+
 export function Step5() {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext<ReadingForm>();
+
   return (
-    <div>
-      <h1>Step 5</h1>
-    </div>
+    <Wrapper>
+      <Controller
+        control={control}
+        name="isPublic"
+        render={({ field }) => (
+          <SelectField
+            label="공개 여부"
+            {...field}
+            value={field.value ? 'true' : 'false'}
+            onChange={e => {
+              field.onChange(e.target.value === 'true');
+            }}
+            error={errors.isPublic?.message}
+          >
+            <option value="true">공개</option>
+            <option value="false">비공개</option>
+          </SelectField>
+        )}
+      />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
